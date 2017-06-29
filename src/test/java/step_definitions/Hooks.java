ccfb7854e36ce62aed87a63540661b3bfa61f18c
pphilip <philip.Philip@objectconsulting.com.au>
2017-06-29 14:25:25 +1000

    changing setProper to C:/Users/Administrator/selenium/chromedriver.exe

--- src/test/java/step_definitions/Hooks.java
+++ src/test/java/step_definitions/Hooks.java
@@ -37,7 +37,7 @@ public class Hooks {
 
 		// System.setProperty("webdriver.chrome.driver",
 		// "C:/Users/PPhilip/Projects/core-automation-testing/drivers/chrome-2.29-winx32/chromedriver.exe");
-		System.setProperty("webdriver.chrome.driver", "C:/Users/Administrator/selenium ");
+		System.setProperty("webdriver.chrome.driver", "C:/Users/Administrator/selenium/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
