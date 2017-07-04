ceb1508b7eecf40cf30381477e83b6fde3f7bdb2
Lei Wang <lwang@objectconsulting.com.au>
2017-07-04 16:07:52 +1000

    change path

--- src/test/java/step_definitions/RunCukesTest.java
+++ src/test/java/step_definitions/RunCukesTest.java
@@ -8,7 +8,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "classpath:features/core",
+		features = "classpath:features",
 		plugin = {"pretty", "html:target/cucumber-html-report"},
 		tags = {}
 		)
